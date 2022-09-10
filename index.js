6e00228e0d3538c5fc59b2c9e3d58d021a052795
Veronika Luthar <justapinglet@gmail.com>
2022-09-10 13:02:39 +1200

    Upload

---
+++ index.js
@@ -0,0 +1,19 @@
+﻿// JavaScript source code
+
+let emojis = ['🙃', '😀', '😂', '😁', '😄', '🤑', '😜'];
+
+function tilde() {
+    for (let i = 0; i < 200; i++) {
+
+        let div = document.createElement('div');
+        div.classList = 'grid-div';
+        div.innerHTML = '~';
+        document.getElementById("grid").appendChild(div);
+
+        div.addEventListener("mouseover", function () {
+            let random = Math.floor(Math.random() * emojis.length);
+            let emoji = emojis[random];
+            div.innerHTML = emoji;
+        });
+    }
+}
